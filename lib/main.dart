59225f1e142b94fa79d849f7c6b34fe2905e1412
Enigma-I-am <nwagba001@gmail.com>
2020-05-11 17:49:59 +0100

    main commit

---
+++ lib/main.dart
@@ -0,0 +1,128 @@
+import 'dart:convert';
+
+import 'package:chopper/chopper.dart';
+import 'package:flutter/material.dart';
+import 'package:learning_chopper/service/post_api_service.dart';
+import 'package:learning_chopper/single_post_page.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Provider(
+      create: (context) => PostApiService.create(),
+      dispose: (context, PostApiService service) => service.client.dispose(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+          // This makes the visual density adapt to the platform that you run
+          // the app on. For desktop platforms, the controls will be smaller and
+          // closer together (more dense) than on mobile platforms.
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+ 
+    return Scaffold(
+      appBar: AppBar(
+      
+        title: Text("learning chopper"),
+      ),
+      body: _buildBody(context),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          // The JSONPlaceholder API always responds with whatever was passed in the POST request
+          final response = await Provider.of<PostApiService>(context)
+              .postPost({'key': 'value'});
+          // We cannot really add any new posts using the placeholder API,
+          // so just print the response to the console
+          print(response.body);
+        },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  FutureBuilder<Response> _buildBody(BuildContext context) {
+    // FutureBuilder is perfect for easily building UI when awaiting a Future
+    // Response is the type currently returned by all the methods of PostApiService
+    return FutureBuilder<Response>(
+      // In real apps, use some sort of state management (BLoC is cool)
+      // to prevent duplicate requests when the UI rebuilds
+      future: Provider.of<PostApiService>(context).getPosts(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          // Snapshot's data is the Response
+          // You can see there's no type safety here (only List<dynamic>)
+          final List posts = json.decode(snapshot.data.bodyString);
+          return _buildPosts(context, posts);
+        } else {
+          // Show a loading indicator while waiting for the posts
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+
+  ListView _buildPosts(BuildContext context, List posts) {
+    return ListView.builder(
+      itemCount: posts.length,
+      padding: EdgeInsets.all(8),
+      itemBuilder: (context, index) {
+        return Card(
+          elevation: 4,
+          child: ListTile(
+            title: Text(
+              posts[index]['title'],
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            subtitle: Text(posts[index]['body']),
+            onTap: () => _navigateToPost(context, posts[index]['id']),
+          ),
+        );
+      },
+    );
+  }
+
+  void _navigateToPost(BuildContext context, int id) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => SinglePostPage(postId: id),
+      ),
+    );
+  }
+}
---
+++ lib/service/post_api_service.dart
@@ -0,0 +1,32 @@
+import 'package:chopper/chopper.dart';
+
+part 'post_api_service.chopper.dart';
+
+@ChopperApi(baseUrl: '/posts')
+abstract class PostApiService extends ChopperService {
+  static PostApiService create() {
+    final client = ChopperClient(
+      baseUrl: 'https://jsonplaceholder.typicode.com',
+      services: [
+        _$PostApiService(),
+      ],
+      converter: JsonConverter(),
+    );
+
+    return _$PostApiService(client);
+  }
+
+  @Get()
+  Future<Response> getPosts();
+
+  @Get(path: '/{id}')
+  // Query parameters are specified the same way as @Path
+  // but obviously with a @Query annotation
+  Future<Response> getPost(@Path('id') int id);
+
+  // Put & Patch requests are specified the same way - they must contain the @Body
+  @Post()
+  Future<Response> postPost(
+    @Body() Map<String, dynamic> body,
+  );
+}
